7034aeddc202b597a2d8fca754ae3a4c50a458b1
Deimos <maxsykora12@gmail.com>
2024-10-03 14:47:05 +0200

    BevyLoadCollection trait

--- crates/core_assets/src/bevy_api.rs
+++ crates/core_assets/src/bevy_api.rs
@@ -3,6 +3,7 @@ use bevy_asset_loader::asset_collection::AssetCollection;
 use bevy_asset_loader::prelude::{ConfigureLoadingState, LoadingStateAppExt, LoadingStateConfig};
 use crate::prelude::LoadingStates;
 
+// For loading asset collections before loading screen
 pub trait BevyLoadCoreCollection {
     fn load_core_collection<A: AssetCollection>(&mut self) -> &mut Self;
 }
@@ -10,6 +11,20 @@ pub trait BevyLoadCoreCollection {
 impl BevyLoadCoreCollection for App {
     fn load_core_collection<A: AssetCollection>(&mut self) -> &mut Self {
         self.configure_loading_state(LoadingStateConfig::new(LoadingStates::CoreAssets)
+            .load_collection::<A>());
+
+        self
+    }
+}
+
+//
+pub trait BevyLoadCollection {
+    fn load_collection<A: AssetCollection>(&mut self) -> &mut Self;
+}
+
+impl BevyLoadCollection for App {
+    fn load_collection<A: AssetCollection>(&mut self) -> &mut Self {
+        self.configure_loading_state(LoadingStateConfig::new(LoadingStates::ContentLoading)
             .load_collection::<A>());
 
         self
